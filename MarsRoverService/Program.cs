8ef778e7a7577a1701900ddf9a9e46e87c2c551a
Johnson Chua <chua.johnson@gmail.com>
2019-02-25 13:51:48 +0800

    test comment

--- MarsRoverService/Program.cs
+++ MarsRoverService/Program.cs
@@ -12,6 +12,7 @@ namespace MarsRoverService
 {
     public class Program
     {
+        //test comment
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
